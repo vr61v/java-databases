4f09b45b08ec9e474cf5e1075506984a3d2fe0c5
Vr61v <nikita20040503@gmail.com>
2025-06-07 15:24:06 +0300

    feat: spring boot dependencies

---
+++ spring-data/src/main/java/org/vr61v/TestController.java
@@ -0,0 +1,16 @@
+package org.vr61v;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @GetMapping()
+    public String hello() {
+        return "hello";
+    }
+
+}
