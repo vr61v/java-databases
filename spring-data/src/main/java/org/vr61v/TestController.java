51636e6fffecd59013f9275cf0b1c92dd3779208
Vr61v <nikita20040503@gmail.com>
2025-06-07 21:17:26 +0300

    feat: create all crud services

--- spring-data/src/main/java/org/vr61v/TestController.java
+++ spring-data/src/main/java/org/vr61v/TestController.java
@@ -1,16 +1,21 @@
 package org.vr61v;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vr61v.services.crud.AircraftCrudService;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class TestController {
 
-    @GetMapping()
+    private final AircraftCrudService service;
+
+    @GetMapping
     public String hello() {
-        return "hello";
+        return service.findAll().toString();
     }
 
 }
